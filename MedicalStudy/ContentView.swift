64ef2ff68e5b4089c83bf3ed4c05bf9d51bdb675
川内翔一朗 <>
2019-10-24 09:02:13 +0900

    first commit

---
+++ MedicalStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedicalStudy
+//
+//  Created by 川内翔一朗 on 2019/10/24.
+//  Copyright © 2019 川内翔一朗. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
